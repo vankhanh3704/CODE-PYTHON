10ca94fb603f78ce8d45fc952bd7921c88eb4397
Hoàng Khánh Văn <vankhanh3704@gmail.com>
2024-09-20 17:24:00 +0700

    DÃY SỐ NHỊ PHÂN

---
+++ PY02004.py
@@ -0,0 +1,15 @@
+# DÃY SỐ NHỊ PHÂN
+import math
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    a = [int(i) for i in input().split()]
+    cnt = 0
+    for i in range(1, n):
+        if a[i] != a[i - 1]:
+            cnt += 1
+    print(cnt)
+
+
