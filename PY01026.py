153caed6b8a8914883089d6a73523f3443be8f00
Hoàng Khánh Văn <vankhanh3704@gmail.com>
2024-09-20 18:28:46 +0700

    SẮP ĐẶT LẠI XÂU KÝ TỰ

--- PY01026.py
+++ PY01026.py
@@ -4,9 +4,9 @@
 if __name__ == "__main__":
     t = int(input())
     for i in range(t):
+        print("Test " + (i + 1) + ": ", end='')
         s1 = sorted(input())
         s2 = sorted(input())
-        print("TEST " + str(i + 1) + ":", end=' ')
         if s1 == s2 : print("YES")
         else: print("NO")
 
